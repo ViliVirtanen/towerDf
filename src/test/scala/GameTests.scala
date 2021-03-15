5385d82fcba83f1ef366e4079ecc6b887a077aca
Vili <vili.virtanen@aalto.fi>
2021-03-15 20:00:53 +0200

    Unit testing created finally.

--- src/test/scala/GameTests.scala
+++ src/test/scala/GameTests.scala
@@ -1,1 +1,16 @@
+import Game._
 
+import org.scalatest.funsuite.AnyFunSuite
+
+// unit testing
+class SetSuite extends AnyFunSuite {
+  val world = new World("src/main/scala/map1.txt")
+  world.createMap()
+  val game = new Game(world, new Player(100,100))
+  val a = new EasyEnemy((0,0),world,game)
+
+  test("destroy should change enemy loc to 9898") {
+   a.destroy()
+    assert(a.loc == (98,98) )
+  }
+}
